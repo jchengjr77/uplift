e02703dc869d987f16e8868fcfaf1ea3c2fb6393
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-06-13 21:42:47 +0800

    Random friend endpoint completed.

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,50 @@ app.get('/profile', (req, res) => {
     userRef.once('value').then((snapshot) => {
         if (!snapshot.exists()) {
             // couldn't find user with uid
-            return res.status(404).send(`No user found with uid ${uid}`);
+            return res.status(404).send(`No user found with id ${uid}`);
         }
         return res.status(200).json(snapshot.val());
     });
 });
 
-app.get('/other');
+app.get('/random-friend', (req, res) => {
+    const selfID = req.query.self_id;
+    if (selfID == null) {
+        return res.status(400).send('Bad Request: self_id is not provided.');
+    }
+    const userRef = db.ref(`/users/${selfID}`);
+    userRef
+        .once('value')
+        .then((snapshot) => {
+            if (!snapshot.exists()) {
+                // couldn't find user with selfID
+                return -1;
+            }
+            const selfProfile = snapshot.val();
+            const friends = selfProfile.friends;
+            const friendUIDs = Object.keys(friends);
+            // see ../odm.json if confused about the profile's structure
+            const randomFriendUID =
+                friendUIDs[(friendUIDs.length * Math.random()) << 0];
+            return randomFriendUID;
+        })
+        .then((randUID) => {
+            if (randUID == -1) {
+                return res.status(404).send(`No user found with id ${selfID}`);
+            }
+            db.ref(`/users/${randUID}`)
+                .once('value')
+                .then((snapshot) => {
+                    if (!snapshot.exists()) {
+                        // couldn't find random friend from randUID
+                        return res
+                            .status(404)
+                            .send(`No friend found with id ${randUID}`);
+                    }
+                    return res.status(200).json(snapshot.val());
+                });
+        });
+});
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
