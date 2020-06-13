542250b0f2f51f0995b29363cb332edece1ca334
SeanPrendi <sean.prendi@gmail.com>
2020-06-13 01:40:37 -0400

    Moved top bar to component, TODO: Fix responsivity and alignment issues.

--- frontend/src/pages/template.js
+++ frontend/src/pages/template.js
@@ -1,21 +1,11 @@
 import React from "react";
 import "../App.css";
-import { Button } from "reactstrap";
+import TopBar from "../components/topbar"
 
 function Template() {
   return (
     <div className="PageContainer">
-      <div className="TopBar">
-        <div className="LogoContainer">
-          <div className="LeftLogoText">up</div>
-          <div className="RightLogoText">lift</div>
-        </div>
-        <div className="TRButtonContainer">
-          <Button outline className="TopRightButton">
-            profile
-          </Button>
-        </div>
-      </div>
+      <TopBar />
       <div className="BodyContainer"></div>
     </div>
   );
