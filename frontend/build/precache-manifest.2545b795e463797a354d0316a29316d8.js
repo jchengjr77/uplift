a431f7ad6d685e77b144391242d11d39bbcb035a
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-06-14 16:04:56 +0800

    Added back app.js (idk why it was deleted)

---
+++ frontend/build/precache-manifest.2545b795e463797a354d0316a29316d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e770895f9499161edefff6228dd4fc2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd7c4ac09e210dd8074",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5ba893abd668bcc5fe2b",
+    "url": "/static/css/main.8c131842.chunk.css"
+  },
+  {
+    "revision": "8dd7c4ac09e210dd8074",
+    "url": "/static/js/2.3f6e1b64.chunk.js"
+  },
+  {
+    "revision": "a855a7222d00a33eb6846ce89bcfdce5",
+    "url": "/static/js/2.3f6e1b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba893abd668bcc5fe2b",
+    "url": "/static/js/main.b22d8caa.chunk.js"
+  },
+  {
+    "revision": "b953b750b242b0536a14",
+    "url": "/static/js/runtime-main.76469584.js"
+  }
+]);
